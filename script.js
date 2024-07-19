2839487fb33c05d7dfdb935dab1265095de7f946
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-19 09:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+function volume_sphere(event) {
+  event.preventDefault();
+  let radius = document.getElementById("radius").value;
+  radius = Number(radius);
+  if (isNaN(radius) || radius < 0) {
+    document.getElementById("volume").value = "NaN";
+    return;
+  }
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  document.getElementById("volume").value = volume.toFixed(4);
+}
+window.onload = function() {
+  document.getElementById('MyForm').onsubmit = volume_sphere;
+};
